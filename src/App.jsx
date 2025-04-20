4c27b9d95d6ced123a2a2541fd19156e0149e627
MOAZZEM HOSEN SARDAR <moazzemhosen9@gmail.com>
2025-04-21 00:03:59 +0530

    GTM Added

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,21 @@ import MainLogin from "./pages/MainLogin";
 import DataCollection from "./pages/DataCollection";
 import UploadData from "./pages/UploadData";
 import NotReadyPage from "./pages/NotReadyPage";
+import TagManager from "react-gtm-module"
+import { useEffect } from "react";
+
+const GTM_ID = 'GTM-T847NLT4'
 
 
 function App() {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: GTM_ID });
+  }, [])
   return (
-   
+
     <Router>
       <Routes>
-        <Route path="/login" element={<MainLogin/>} />
+        <Route path="/login" element={<MainLogin />} />
         <Route element={<ProtectedRoute />}>
 
           <Route path="/" element={<HomePage />} />
@@ -23,12 +30,12 @@ function App() {
           <Route path="/reportDashboard" element={<NotReadyPage />} />
           <Route path="/userManagement" element={<NotReadyPage />} />
           <Route path="/otherSection" element={<NotReadyPage />} />
-          <Route path="/downloadtemplate" element={<DownloadTemp/>} />
-          <Route path="/uploaddata" element={<UploadData/>} />
+          <Route path="/downloadtemplate" element={<DownloadTemp />} />
+          <Route path="/uploaddata" element={<UploadData />} />
         </Route>
       </Routes>
     </Router>
-  
+
   );
 }
 
