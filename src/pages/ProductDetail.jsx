805eaed76bebd0ed7a68db4ec1a34b6ce0b6a835
MOAZZEM HOSEN SARDAR <moazzemhosen9@gmail.com>
2025-05-07 23:42:49 +0530

    FormId

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -204,6 +204,7 @@ const ProductDetail = () => {
                                 </select>
                             </div>
                             <button
+                            id='PlaceOrder'
                                 type="button"
                                 onClick={handlePlaceOrder}
                                 className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded"
